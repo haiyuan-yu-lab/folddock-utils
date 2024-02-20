36dc43869c087bd092e0d1e52bfc734d6d7c2476
Mateo Torres <torresmateo@gmail.com>
2024-02-20 14:19:04 -0500

    get times can handle multiple files per interaction

--- get_times.py
+++ get_times.py
@@ -21,17 +21,74 @@ def get_time_and_memory(outfile: Path) -> Tuple:
                     times[key][event] = dt
                 else:
                     memory = [float(i) for i in line.strip()[1:-1].split(",")]
-    return memory, times         
+    return memory, times
+
+
+def get_resource_usages(resources, p1_res, p2_res):
+    memory_max = []
+    memory_min = []
+    memory_mean = []
+    stages = ['UNALIGN', 'CDHIT', 'oxmatch', 'fuse_msas', 'AlphaFold 1',
+              'AlphaFold 2', 'AlphaFold 3', 'AlphaFold 4', 'AlphaFold 5']
+    times = {stage: [] for stage in stages}
+    for res in resources:
+        times = res["times"]
+        memory_min.append(min(res["mem_array"]))
+        memory_mean.append(np.mean(res["mem_array"]))
+        memory_max.append(max(res["mem_array"]))
+        feature_duration = 0
+        for stage in stages:
+            try:
+                duration = times[stage]["end"] - times[stage]["start"]
+                times[stage].append(duration)
+                if stage.startswith("Alpha"):
+                    prediction_duration += duration.seconds
+                else:
+                    feature_duration += duration.seconds
+            except KeyError:
+                print(stage, times)
+        time_hhblits = p1_res["times"]["HHBLITS"]
+        duration_hhblits = (time_hhblits["end"]
+                            - time_hhblits["start"]).seconds
+        time_hhblits = p2_res["times"]["HHBLITS"]
+        duration_hhblits += (time_hhblits["end"]
+                             - time_hhblits["start"]).seconds
+        feature_duration += duration_hhblits
+        feature_duration += (max(times["UNALIGN"])
+                             + max(times["CDHIT"])
+                             + max(times["oxmatch"]))
+        prediction_duration = (max(times["AlphaFold 1"]),
+                               + max(times["AlphaFold 2"])
+                               + max(times["AlphaFold 3"])
+                               + max(times["AlphaFold 4"])
+                               + max(times["AlphaFold 5"]))
+    return (
+        max(memory_min),
+        max(memory_mean),
+        max(memory_max),
+        max(times["UNALIGN"]),
+        max(times["CDHIT"]),
+        max(times["oxmatch"]),
+        max(times["fuse_msas"]),
+        max(times["AlphaFold 1"]),
+        max(times["AlphaFold 2"]),
+        max(times["AlphaFold 3"]),
+        max(times["AlphaFold 4"]),
+        max(times["AlphaFold 5"]),
+        feature_duration,
+        prediction_duration,
+        feature_duration + prediction_duration)
 
 
 def run(folddock_output_dir: Path,
-        msa_output_dir: Path, 
-        interactions_file: Path, 
+        msa_output_dir: Path,
+        interactions_file: Path,
         output_file: Path
         ):
     assert folddock_output_dir.is_dir()
     assert interactions_file.is_file()
-    interactions = sorted([l.strip().split() for l in interactions_file.open()])    
+    interactions = sorted([line.strip().split()
+                           for line in interactions_file.open()])
     print(f"processing {len(interactions)} interactions")
     proteins = set()
     for p1, p2 in interactions:
@@ -49,61 +106,80 @@ def run(folddock_output_dir: Path,
             if prot in proteins and prot not in protein_to_results:
                 protein_to_results[prot] = outfile
     print(f"found {len(protein_to_results)} protein result files")
+
     interaction_to_results = {}
+    files_found = 0
     for outfile in sorted(folddock_output_dir.glob("**/*.out")):
         with outfile.open() as of:
             try:
                 p1, p2 = of.readline().split("=")[-1].strip().split()
             except ValueError:
                 continue
-            if [p1, p2] in interactions and (p1, p2) not in interaction_to_results:
-                interaction_to_results[p1,p2] = outfile
-    print(f"found {len(interaction_to_results)} result files")
+            if (p1, p2) not in interaction_to_results:
+                interaction_to_results[p1, p2] = []
+            if [p1, p2] in interactions:
+                interaction_to_results[p1, p2].append(outfile)
+                files_found += 1
+    print(f"found {len(files_found)} result files")
+
     resources = {}
-    for (p1, p2), outfile in interaction_to_results.items():
-        memory, times = get_time_and_memory(outfile)
-        resources[p1, p2] = {
-            "mem_array": memory,
-            "times": times,
-        }
-    print(list(resources[p1,p2]["times"].keys()))
+    for (p1, p2), outfiles in interaction_to_results.items():
+        for outfile in outfiles:
+            memory, times = get_time_and_memory(outfile)
+            if (p1, p2) not in resources:
+                resources[p1, p2] = {}
+            resources[p1, p2].append({
+                "mem_array": memory,
+                "times": times,
+            })
+    print(list(resources[p1, p2]["times"].keys()))
     for prot, outfile in protein_to_results.items():
         memory, times = get_time_and_memory(outfile)
         resources[prot] = {
-            "mem_array": memory, 
+            "mem_array": memory,
             "times": times
         }
-    stages = ['UNALIGN', 'CDHIT', 'oxmatch', 'fuse_msas', 'AlphaFold 1', 'AlphaFold 2', 'AlphaFold 3', 'AlphaFold 4', 'AlphaFold 5']
     with output_file.open("w") as of:
-        of.write("protein1\tprotein2\tmem min\tmem mean\tmem max\ttime UNALIGN\ttime CDHIT\ttime oxmatch\ttime fuse_msas\ttime AlphaFold 1\ttime AlphaFold 2\ttime AlphaFold 3\ttime AlphaFold 4\ttime AlphaFold 5\ttime HHBLITS\ttime features\ttime prediction\ttime total\n")
+        of.write("protein1\tprotein2\tmem min\tmem mean\tmem max\t"
+                 "time UNALIGN\ttime CDHIT\ttime oxmatch\ttime fuse_msas\t"
+                 "time AlphaFold 1\ttime AlphaFold 2\ttime AlphaFold 3\t"
+                 "time AlphaFold 4\ttime AlphaFold 5\ttime HHBLITS\t"
+                 "time features\ttime prediction\ttime total\n")
         for (p1, p2), _ in interaction_to_results.items():
-            l = f"{p1}\t{p2}"
-            res = resources[p1, p2]
-            times = res["times"]
-            l += f"\t{min(res['mem_array'])}"
-            l += f"\t{np.mean(res['mem_array'])}"
-            l += f"\t{max(res['mem_array'])}"
-            feature_duration = 0
-            prediction_duration = 0
-            for stage in stages:
-                try:
-                    duration = times[stage]["end"] - times[stage]["start"]
-                    l += f"\t{duration.seconds}"
-                    if stage.startswith("Alpha"):
-                        prediction_duration += duration.seconds     
-                    else:
-                        feature_duration += duration.seconds    
-                except KeyError:
-                    print(stage, times)
-                    l += "\t-1"
-            time_hhblits = resources[p1]["times"]["HHBLITS"]
-            duration_hhblits = (time_hhblits["end"] - time_hhblits["start"]).seconds
-            time_hhblits = resources[p2]["times"]["HHBLITS"]
-            duration_hhblits += (time_hhblits["end"] - time_hhblits["start"]).seconds
-            feature_duration += duration_hhblits
-            l += f"\t{duration_hhblits}\t{feature_duration}\t{prediction_duration}\t{feature_duration + prediction_duration}\n"
-            of.write(l)
-            
+            (memory_min,
+             memory_mean,
+             memory_max,
+             timesUNALIGN,
+             timesCDHIT,
+             timesoxmatch,
+             timesfuse_msas,
+             timesAlphaFold1,
+             timesAlphaFold2,
+             timesAlphaFold3,
+             timesAlphaFold4,
+             timesAlphaFold5,
+             feature_duration,
+             prediction_duration,
+             total_duration) = get_resource_usages(resources[p1, p2],
+                                                   resources[p1],
+                                                   resources[p2])
+            of.write(f"{p1}\t{p2}"
+                     f"\t{memory_min}"
+                     f"\t{memory_mean}"
+                     f"\t{memory_max}"
+                     f"\t{timesUNALIGN}"
+                     f"\t{timesCDHIT}"
+                     f"\t{timesoxmatch}"
+                     f"\t{timesfuse_msas}"
+                     f"\t{timesAlphaFold1}"
+                     f"\t{timesAlphaFold2}"
+                     f"\t{timesAlphaFold3}"
+                     f"\t{timesAlphaFold4}"
+                     f"\t{timesAlphaFold5}"
+                     f"\t{feature_duration}"
+                     f"\t{prediction_duration}"
+                     f"\t{total_duration}\n")
+
 
 if __name__ == "__main__":
 
